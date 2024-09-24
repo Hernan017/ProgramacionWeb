0e1c1f1d83fe257282e7904aa6482c2c5b73eb37
Hernan017 <hanguloc2200@alumno.ipn.mx>
2024-09-24 17:57:34 -0600

    Creacion de directorio y archivo

---
+++ script.js
@@ -0,0 +1,28 @@
+//Clase 24/09/2024
+//Nombre: Angulo Cayetano Hernan Manuel
+
+/*
+Las variables guardn info que puedes reutilizar
+*/
+
+//Declaracion de variables
+
+let nombre = 'Pedro'; //usa let para declarar variables que pueden cambiar su valor
+const PI = 3.1416;
+var edad = 25;
+
+//operadores
+let a = 5;
+let b = 2;
+
+let suma = a + b;// 7
+let resta = a - b;//3
+let multiplicacion = a * b;//10
+let division = a / b;//2.5
+
+
+//operadores de comparación
+a > b; //true
+a < b; //false
+a == b; //false
+a === b; //false (compara valor y tipo)
