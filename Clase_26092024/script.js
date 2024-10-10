dbc645d57fc96c42c840e880b95547a05317484c
Hernan017 <hanguloc2200@alumno.ipn.mx>
2024-10-10 16:09:54 -0600

    Creo que ya

--- Clase_26092024/script.js
+++ Clase_26092024/script.js
@@ -1,22 +1,19 @@
 function appendToDisplay(value){
     document.getElementById('display').value += value;
 }
-
 function clearDisplay(){
-    document.getElementById('display').value += '';
+    document.getElementById('display').value = '';
 }
-
 function deleteLast(){
-    let displayValue = document.getElementById('diplay').value;
-    document.getElementById('display').value = displayValue.slice(0, -1);
+    let displayValue = document.getElementById('display').value;
+    document.getElementById('display').value = displayValue.slice(0,-1);
 }
 
 function calculateResult(){
     try{
         let result = eval(document.getElementById('display').value);
         document.getElementById('display').value = result;
-    } catch (error) {
-        alert('Expresion inválida');
+    }  catch(error){
+        alert('Expresion invalida');
     }
-
 }
